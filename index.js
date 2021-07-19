250a4eeb9025167395674bdf2336fb2c4a966e6c
ufo <ufo@localhost.localdomain>
2021-07-19 03:15:43 -0400

    need to test

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Auth, LoginCredentials } = require("two-step-auth");
+  
+async function login(emailId) {
+  try {
+    const res = await Auth(emailId, "Company Name");
+    console.log(res);
+    console.log(res.mail);
+    console.log(res.OTP);
+    console.log(res.success);
+  } catch (error) {
+    console.log(error.LoginCredentials);
+  }
+}
+
+LoginCredentials.mailID = "email"; 
+
+LoginCredentials.password = "password"; 
+LoginCredentials.use = true;
+
+  
+login("mrshk.osperb@gmail.com");
